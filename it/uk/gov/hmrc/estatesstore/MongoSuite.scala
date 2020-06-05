35165a7f274c4da42da3fb43655f53668a5ba973
Adam Conder <adamdconder@gmail.com>
2020-06-05 15:23:58 +0100

    Changing to FutureFlatSpec to not to .get on assertion

--- it/uk/gov/hmrc/estatesstore/MongoSuite.scala
+++ it/uk/gov/hmrc/estatesstore/MongoSuite.scala
@@ -1,18 +1,20 @@
 package uk.gov.hmrc.estatesstore
 
 import org.scalatest.TestSuite
-import org.scalatest.concurrent.{IntegrationPatience, PatienceConfiguration}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
 import play.api.Application
+import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.api.MongoConnection
+import uk.gov.hmrc.estatesstore.repositories.{EstatesMongoDriver, RegisterTasksRepository, TasksRepository}
 
-import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
 
-trait MongoSuite extends IntegrationPatience {
-  self: TestSuite with PatienceConfiguration =>
+trait MongoSuite extends ScalaFutures {
+
+  implicit val defaultPatience: PatienceConfig = PatienceConfig(timeout = Span(30, Seconds), interval = Span(500, Millis))
 
   // Database boilerplate
   private val connectionString = "mongodb://localhost:27017/estates-store-integration"
@@ -24,24 +26,25 @@ trait MongoSuite extends IntegrationPatience {
   def getConnection(application: Application) = {
     val mongoDriver = application.injector.instanceOf[ReactiveMongoApi]
 
-    lazy val connection = for {
+    for {
       uri <- MongoConnection.parseURI(connectionString)
       connection <- mongoDriver.driver.connection(uri, true)
     } yield connection
-    connection
   }
 
-  def dropTheDatabase(connection: MongoConnection): Unit = {
-    Await.result(getDatabase(connection).flatMap(_.drop()), Duration.Inf)
+  def dropTheDatabase(connection: MongoConnection) = {
+    getDatabase(connection).flatMap(_.drop())
   }
 
-  val application : Application = new GuiceApplicationBuilder()
+  def application : Application = new GuiceApplicationBuilder()
     .configure(Seq(
       "mongodb.uri" -> connectionString,
       "metrics.enabled" -> false,
       "auditing.enabled" -> false,
       "mongo-async-driver.akka.log-dead-letters" -> 0
     ): _*)
-    .build()
+    .overrides(
+      bind[TasksRepository].to(classOf[RegisterTasksRepository])
+    ).build()
 
 }
--- it/uk/gov/hmrc/estatesstore/repositories/RegisterTasksRepositorySpec.scala
+++ it/uk/gov/hmrc/estatesstore/repositories/RegisterTasksRepositorySpec.scala
@@ -2,86 +2,75 @@ package uk.gov.hmrc.estatesstore.repositories
 
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
+import play.api.Application
 import play.api.test.Helpers._
 import uk.gov.hmrc.estatesstore.MongoSuite
 import uk.gov.hmrc.estatesstore.models.register.{TaskCache, Tasks}
 
+import scala.concurrent.Future
 import scala.language.implicitConversions
 
-class RegisterTasksRepositorySpec extends FreeSpec with MustMatchers
+class RegisterTasksRepositorySpec extends AsyncFreeSpec with MustMatchers
   with ScalaFutures with OptionValues with MongoSuite {
+
+  val internalId = "Int-328969d0-557e-4559-96ba-074d0597107e"
+
+  def assertMongoTest(application: Application)(block: => Assertion) : Future[Assertion] = for {
+      connection <- Future.fromTry(getConnection(application))
+      _ <- dropTheDatabase(connection)
+      result <- block
+    } yield {
+      application.stop()
+      result
+    }
 
   "a tasks repository" - {
 
-    val internalId = "Int-328969d0-557e-4559-96ba-074d0597107e"
-
-    "must return None when no cache exists" in {
-      running(application) {
-
-        getConnection(application).map{ connection =>
-          dropTheDatabase(connection)
-
-          val repository = application.injector.instanceOf[RegisterTasksRepository]
-
-          repository.get[TaskCache](internalId).futureValue mustBe None
-        }
+    "must return None when no cache exists" in running(application) {
+      assertMongoTest(application) {
+        val repository = application.injector.instanceOf[TasksRepository]
+        repository.get[TaskCache](internalId).futureValue mustBe None
       }
     }
 
-    "must return TaskCache when one exists" in {
-      running(application) {
+    "must return TaskCache when one exists" in running(application) {
+      assertMongoTest(application) {
+        val repository = application.injector.instanceOf[TasksRepository]
 
-          getConnection(application).map { connection =>
-            dropTheDatabase(connection)
+        val task = Tasks(details = false, personalRepresentative = false, deceased = false, yearsOfTaxLiability = false)
 
-            val repository = application.injector.instanceOf[RegisterTasksRepository]
+        val cache = TaskCache(internalId, task)
 
-            val task = Tasks(details = false, personalRepresentative = false, deceased = false, yearsOfTaxLiability = false)
+        val initial = repository.set(internalId, cache).futureValue
 
-            val cache = TaskCache(internalId, task)
+        initial mustBe true
 
-            val initial = repository.set(internalId, cache).futureValue
-
-            initial mustBe true
-
-            repository.get[TaskCache](internalId).futureValue.value.tasks mustBe task
-
-            dropTheDatabase(connection)
-        }
+        repository.get[TaskCache](internalId).futureValue.value.tasks mustBe task
       }
     }
 
-    "must set an updated Task and return one that exists for that user" in {
-      running(application) {
+    "must set an updated Task and return one that exists for that user" in running(application) {
+      assertMongoTest(application) {
+        val repository = application.injector.instanceOf[TasksRepository]
 
-        getConnection(application).map { connection =>
-          dropTheDatabase(connection)
+        val task = Tasks(details = true, personalRepresentative = false, deceased = false, yearsOfTaxLiability = false)
 
-          val repository = application.injector.instanceOf[RegisterTasksRepository]
+        val initial = TaskCache(internalId, task)
 
-          val task = Tasks(details = true, personalRepresentative = false, deceased = false, yearsOfTaxLiability = false)
+        repository.set(internalId, initial).futureValue
 
-          val initial = TaskCache(internalId, task)
+        repository.get[TaskCache](internalId).futureValue.value.tasks mustBe task
 
-          repository.set(internalId, initial).futureValue
+        // update
 
-          repository.get[TaskCache](internalId).futureValue.value.tasks mustBe initial
+        val updatedTask = Tasks(details = true, personalRepresentative = true, deceased = false, yearsOfTaxLiability = false)
 
-          // update
+        val updatedCache = TaskCache(internalId, updatedTask)
 
-          val updatedTask = Tasks(details = true, personalRepresentative = true, deceased = false, yearsOfTaxLiability = false)
+        repository.set(internalId, updatedCache).futureValue
 
-          val updatedCache = TaskCache(internalId, updatedTask)
-
-          repository.set(internalId, updatedCache).futureValue
-
-          repository.get[TaskCache](internalId).futureValue.value.tasks mustBe updatedTask
-
-          dropTheDatabase(connection)
-        }
-
+        repository.get[TaskCache](internalId).futureValue.value.tasks mustBe updatedTask
       }
     }
-
   }
 }
