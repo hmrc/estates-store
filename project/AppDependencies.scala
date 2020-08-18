ab10a3c879744d3dd8d194c616187099a9d41f57
Adam Conder <adamdconder@gmail.com>
2020-08-18 18:25:31 +0100

    Upgrade test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,4 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
@@ -22,14 +20,13 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.8.0" % Test classifier "tests",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.14.0" % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
     "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.24.1"                % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
-    "org.mockito"             %  "mockito-core"             % "1.10.19"               % "test, it"
+    "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test, it"
   )
 
 }
