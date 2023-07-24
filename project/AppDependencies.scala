7bb2ff65228aa5663308b3e5122b0d09e8e72176
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2023-07-24 08:22:40 +0100

    DDCE-4075: Upgrade to Scala 2.13.10. Added unit tests for MongoDateTimeFormats.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val mongoHmrcVersion = "0.73.0"
-  private val playBootstrapVersion = "7.7.0"
+  private val mongoHmrcVersion = "1.3.0"
+  private val playBootstrapVersion = "7.20.0"
 
   val compile = Seq(
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % mongoHmrcVersion,
@@ -14,11 +14,11 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % playBootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % mongoHmrcVersion,
-    "com.typesafe.play"       %% "play-test"                  % current,
-    "org.scalatest"           %% "scalatest"                  % "3.2.14",
-    "org.scalatestplus"       %% "mockito-4-6"                % "3.2.14.0",
+    "com.typesafe.play"       %% "play-test"                  % "2.8.20",
+    "org.scalatest"           %% "scalatest"                  % "3.2.16",
+    "org.scalatestplus"       %% "mockito-4-6"                % "3.2.15.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
-    "com.vladsch.flexmark"    % "flexmark-all"                % "0.62.2"
+    "com.vladsch.flexmark"    % "flexmark-all"                % "0.64.8"
   ).map(_ % "test, it")
 
 }
